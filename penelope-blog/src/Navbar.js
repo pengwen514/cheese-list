64be3679e79877a26fa42bd183f14f65e6242703
Penelope <phuff110@gmail.com>
2023-06-05 11:46:02 -0500

    added navbar and homepage for future routes

---
+++ penelope-blog/src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav>
+            <h1>Penelope's React Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
